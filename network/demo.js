54a839dd9d90df86d9e01ef50552b7ec42660d31
veu <becky@monometric.net>
2015-02-16 18:56:24 +0100

    make 'infection rate' depend on color difference

--- network/demo.js
+++ network/demo.js
@@ -16,9 +16,7 @@ var
     // minimum radius for hype circles
     minRadius = 10,
     // maximum radius for hype circles
-    maxRadius = 50,
-    // likelyhood to get hyped if touched by hype circle
-    hypeRate = 0.2;
+    maxRadius = 50;
 
 var nodes = [],
     hype = [];
@@ -37,17 +35,13 @@ function draw() {
         node.move();
         c.beginPath();
         c.arc(node.x, node.y, node.drawRadius(), 0, 7, 0);
-        // c.fillStyle = node.hyped ? '#fff' : '#aaf';
         c.fillStyle = getColor(node.color);
         c.fill();
-        // c.strokeStyle = '#000';
-        // c.stroke();
     }
 
     for (i in hype) {
         c.beginPath();
         c.arc(hype[i].node.x, hype[i].node.y, hype[i].r, 0, 7, 0); // hype moves with node.
-        // c.arc(hype[i].x, hype[i].y, hype[i].r, 0, 7, 0);
         c.fillStyle = getColor(hype[i].node.color, (1 - hype[i].r / hype[i].rMax) * .5); //'#f00';
         c.fill();
         c.strokeStyle = getColor(hype[i].node.color); //'#f00';
@@ -62,14 +56,11 @@ function update() {
         for (k in nodes) {
             node = nodes[k];
             if (node.hyped > 0 || Math.abs(node.distance(hype[i]) - hype[i].r) > 2) continue;
-            if (Math.random() < hypeRate) {
-                node.hype(hype[i]);
-            }
+            node.tryHype(hype[i]);
         }
     }
     hype = hype.filter(function (h) {
         var alive = h.r < h.rMax;
-        // if (!alive) h.node.hyped = 0;
         return alive;
     });
     draw();
@@ -97,7 +88,7 @@ document.onclick = function (e) {
     for (var y = gridHeight; y--;) {
         for (var x = gridWidth; x--;) {
             nodes.push({
-                color: Math.random() * 255 | 0,
+                color: Math.random() * 256 | 0,
                 x: x * spread + offset + (Math.random() - .5) * 12,
                 y: y * spread + offset + (Math.random() - .5) * 12,
                 vx: 0, vy: 0,
@@ -105,9 +96,18 @@ document.onclick = function (e) {
                 move: function() { this.x += this.vx; this.y += this.vy; this.vx *= .9; this.vy *= .9; },
                 drawRadius: function () { return this.r / maxRadius * 6; },
                 distance: function (p) { return Math.sqrt((x = this.x - p.x) * x + (y = this.y - p.y) * y, 2); },
+                tryHype: function (hyper) {
+                    var colorDiff = Math.abs(this.color - hyper.node.color);
+                    if (Math.random() < (1 - colorDiff / 256) / 4) {
+                        this.hype(hyper);
+                    }
+                },
                 hype: function (hyper) {
                     // a hyper hypes a hypee
-                    if (hyper.node) this.color = (hyper.node.color + (Math.random() - .5) * 20) | 0; // adopt color with slight mutation
+                    if (hyper.node) {
+                        this.color = (hyper.node.color + (Math.random() - .5) * 20) | 0;
+                        this.color = Math.max(0, Math.min(255, this.color));
+                    } // adopt color with slight mutation
                     this.vx = -(this.x - hyper.x) * .05;
                     this.vy = -(this.y - hyper.y) * .05;
                     this.hyped = 1;
