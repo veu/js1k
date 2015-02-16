f4f50942c488c134147ba880986dc7d723f00d59
veu <becky@monometric.net>
2015-02-16 13:50:24 +0100

    version with different levels / colors

--- network/demo.js
+++ network/demo.js
@@ -1,104 +1,139 @@
 // config
 var
     // size for area with nodes
-    width = 600,
-    height = 400,
+    gridWidth = a.width,
+    gridHeight = a.height,
     // top and left offset
-    offset = 20,
+    gridOffset = 20,
 
-    // number of columns
-    columns = 24,
-    // number of rows
-    rows = 16,
+    // space between nodes
+    nodeSpread = 30,
+    // randomized deviation from the grid
+    nodeDeviation = 16,
+
+    // boost for new waves
+    waveBoost = 1,
+    // reduction when wave hits node with lower score
+    waveDrop = 1.5,
 
     // simulation speed
-    fps = 30,
+    fps = 30;
 
-    // minimum radius for hype circles
-    minRadius = 10,
-    // maximum radius for hype circles
-    maxRadius = 50,
-    // likelyhood to get hyped if touched by hype circle
-    hypeRate = 0.2;
+var nodes,
+    hypeWaves,
+    rand = Math.random;
 
+function createNode(x, y) {
+    return {
+        x: x * nodeSpread + gridOffset + (rand() - .5) * nodeDeviation,
+        y: y * nodeSpread + gridOffset + (rand() - .5) * nodeDeviation,
+        level: rand() < 0.5 ? 1 : rand() < 0.7 ? 2 : 3,
+        hyped: 0,
+        r: 8,
+        distance: function (p) {
+            var x = this.x - p.x,
+                y = this.y - p.y;
+            return Math.sqrt(x * x + y * y, 2);
+        },
+        tryHype: function (wave) {
+            if (this.hyped) return;
+            if (this.level > wave.level()) return;
+            if (this.level < wave.level()) {
+                wave._level -= waveDrop;
+            }
+            this.hype(wave);
+        },
+        hype: function (wave) {
+            this.hyped = 1;
+            hypeWaves.push(w = createWave(this, wave));
+        }
+    }
+}
 
-var nodes, hypeWaves;
+function createWave(node, wave) {
+    var level = Math.min(wave._level + waveBoost, 5);
+    return {
+        x: node.x,
+        y: node.y,
+        r: 8,
+        rMax: 18 + level * 5,
+        _level: level,
+        level: function () {
+            return Math.max(this._level + 1 >> 1, 1);
+        }
+    }
+}
 
 function setup() {
-    nodes = [],
+    var x, y,
+        columns = (gridWidth - gridOffset * 2) / nodeSpread + 1 | 0,
+        rows = (gridHeight - gridOffset * 2) / nodeSpread + 1 | 0;
+    nodes = [];
     hypeWaves = [];
-    for (var y = rows; y--;) {
-        for (var x = columns; x--;) {
-            nodes.push({
-                x: (x / columns * width) + offset + (Math.random() - .5) * 12,
-                y: (y / rows * height) + offset + (Math.random() - .5) * 12,
-                r: (Math.random() * (maxRadius - minRadius) | 0) + minRadius,
-                drawRadius: function () { return this.r / maxRadius * 6; },
-                distance: function (p) { return Math.sqrt((x = this.x - p.x) * x + (y = this.y - p.y) * y, 2); },
-                hype: function () { this.hyped = 1; hypeWaves.push({x: this.x, y: this.y, r: 0, rMax: this.r}); }
-            });
+    for (y = rows; y--;) {
+        for (x = columns; x--;) {
+            if (rand() < .2) continue;
+            nodes.push(createNode(x, y));
         }
     }
     setInterval(update, 1000 / fps | 0);
 }
 
 function draw() {
-    var i, node, wave;
+    var x, y;
+    c.font = '27px Arial';
+    c.textAlign = 'center';
     c.fillStyle = '#000';
     c.fillRect(0, 0, a.width, a.height);
 
-    for (i in nodes) {
-        node = nodes[i];
+    nodes.forEach(function (node) {
         c.beginPath();
-        c.arc(node.x, node.y, node.drawRadius(), 0, 7, 0);
-        c.fillStyle = node.hyped ? '#fff' : '#aaf';
+        x = node.x + (node.hyped && rand());
+        y = node.y + (node.hyped && rand());
+        c.arc(x, y, node.r, 0, 7, 0);
+        c.fillStyle = '#' + ['fff', 'afa', 'ffa', 'faa'][node.hyped ? 0 : node.level];
         c.fill();
-        c.strokeStyle = '#000';
-        c.stroke(); 
-    }
+        c.fillStyle = '#000';
+        c.fillText('☺', x, y + 7);
+    });
 
-    for (i in hypeWaves) {
-        wave = hypeWaves[i];
+    hypeWaves.forEach(function (wave) {
         c.beginPath();
         c.arc(wave.x, wave.y, wave.r, 0, 7, 0);
-        c.strokeStyle = '#f00';
+        c.strokeStyle = '#' + ['0f0', 'ff0', 'f00'][wave.level() - 1];
         c.stroke(); 
-    }
+    });
 }
 
 function update() {
-    var i, k, node, wave;
-    for (i in hypeWaves) {
-        wave = hypeWaves[i];
+    hypeWaves.forEach(function (wave) {
         wave.r++;
-        for (k in nodes) {
-            node = nodes[k];
-            if (node.hyped || Math.abs(node.distance(wave) - wave.r) > 2) continue;
-            if (Math.random() < hypeRate) {
-                node.hype()
+        nodes.forEach(function (node) {
+            if (Math.abs(node.distance(wave) - wave.r) < 0.5) {
+                node.tryHype(wave);
             }
-        }
-    }
-    hypeWaves = hypeWaves.filter(function (h) { return h.r < h.rMax; });
+        });
+    });
+    hypeWaves = hypeWaves.filter(function (wave) {
+        return wave.level() > 0 && wave.r < wave.rMax;
+    });
     draw();
 }
 
 function reset() {
-    for (var i in nodes) {
-        nodes[i].hyped = 0;
-    }
+    nodes.forEach(function (node) {
+        node.hyped = 0;
+    });
 }
 
 document.onclick = function (e) {
-    var i, node, mouse = {x: e.pageX, y: e.pageY};
-    for (i in nodes) {
-        node = nodes[i];
-        if (node.distance(mouse) <= node.drawRadius()) {
-            node.hype();
-            break;
+    var mouse = {x: e.pageX, y: e.pageY};
+    nodes.some(function (node) {
+        if (node.level == 1 && node.distance(mouse) <= node.r) {
+            node.hype({level: function () { return 0; }, _level: 0});
+            return true;
         }
-
-    }
+    });
 };
 
 setup();
