2c912537f77ebc21d9d3aab79a23ea222c73d4e1
veu <becky@monometric.net>
2015-03-05 07:17:25 +0100

    remove the need for 'with Math' (1015B)

--- network/demo.js
+++ network/demo.js
@@ -11,14 +11,14 @@ onclick = function (e, f) {
     showInstructions =
         // check if we clicked on a node
         nodes.some(function (node) {
-            sqrt((x = node.x - e.pageX / cScale + offset) * x + (y = node.y - e.pageY / cScale) * y) > node.radius
+            Math.sqrt((x = node.x - e.pageX / cScale + offset) * x + (y = node.y - e.pageY / cScale) * y) > node.radius
                 || startHype(node, node)
         })
 },
 
 startHype = function (e, f) {
     // adopt color with slight mutation
-    f.color = e.color + (f.color - e.color) / 10 * random() + 10 * random() - 5 | 0,
+    f.color = e.color + (f.color - e.color) / 10 * Math.random() + 10 * Math.random() - 5 | 0,
     // move hypee closer to hyper
     f.vx = (e.x - f.x) / 25,
     f.vy = (e.y - f.y) / 25,
@@ -34,10 +34,10 @@ startHype = function (e, f) {
 e = 600; while (e--) {
     nodes.push({
         // node color
-        color: 256 * random() | 0,
+        color: 256 * Math.random() | 0,
         // position
-        x: e % 30 * 25 + 30 + 20 * random(),
-        y: (e / 30 | 0) * 25 + 30 + 20 * random(),
+        x: e % 30 * 25 + 30 + 20 * Math.random(),
+        y: (e / 30 | 0) * 25 + 30 + 20 * Math.random(),
         // speed
         vx: 0, vy: 0,
         // hype (shaking animation)
@@ -45,7 +45,7 @@ e = 600; while (e--) {
         // hype wave radius
         wave: time = 0,
         // node radius
-        radius: 5 * random() + 1 | 0
+        radius: 5 * Math.random() + 1 | 0
     })
 }
 
@@ -62,12 +62,12 @@ nodes.some(function (node) {
 
         // start new hype after a cooldown
         ++idle - 200 ||
-            startHype(node = nodes[256 * random() | 0], node),
+            startHype(node = nodes[256 * Math.random() | 0], node),
 
         // reset context state, reset color spectrum, and draw background
         c.fillRect(spectrum = [], 0, a.width = a.width, a.height),
         // scale canvas to demo size
-        c.scale(cScale = min(a.width / 804, a.height / 640), cScale),
+        c.scale(cScale = Math.min(a.width / 804, a.height / 640), cScale),
         // center demo
         c.translate(offset = (a.width / cScale - 804) / 2, 0),
 
@@ -107,8 +107,8 @@ nodes.some(function (node) {
                 // hype infecting new nodes
                 nodes.some(function (node) {
                     node.e ||
-                        abs(sqrt((x = node.x - e.x) * x + (y = node.y - e.y) * y, 2) - e.wave) < 2 &&
-                            node.radius * random() < 1 - abs(node.color - e.color) / 256 &&
+                        Math.abs(Math.sqrt((x = node.x - e.x) * x + (y = node.y - e.y) * y, 2) - e.wave) < 2 &&
+                            node.radius * Math.random() < 1 - Math.abs(node.color - e.color) / 256 &&
                             startHype(e, node)
                 }),
 
@@ -129,7 +129,7 @@ nodes.some(function (node) {
         // draw spectrum
         e = 256; while (e--) {
             c.fillStyle = 'rgba(' + [255, 255 - e, e, 1],
-            c.fillRect(147 + e * 2, 610 - log(spectrum[e]) * 5, 2, log(spectrum[e]) * 5)
+            c.fillRect(147 + e * 2, 610 - Math.log(spectrum[e]) * 5, 2, Math.log(spectrum[e]) * 5)
         }
 
         // draw line
@@ -141,7 +141,7 @@ nodes.some(function (node) {
             e = node.color, c.shadowColor = c.fillStyle = 'rgba(' + [255, 255 - e, e, 1],
             c.shadowBlur = node.radius * 3,
             c.beginPath(),
-            c.arc(node.x + node.e * random() / 100, node.y + node.e * random() / 100, node.radius, 0, 7, 0),
+            c.arc(node.x + node.e * Math.random() / 100, node.y + node.e * Math.random() / 100, node.radius, 0, 7, 0),
             c.fill()
         })
     })(e)
